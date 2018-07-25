ac5e336856659de815f32b72fa3c655eeadf777d
James Tietjen <35675497+MrPhuzzles@users.noreply.github.com>
2018-07-24 23:29:08 -0400

    Character Movement
    Created player.cs and scripted left/right movement.

---
+++ TileVania/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    public float runSpeed;
+    private Rigidbody2D myRigidbody;
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Run();
+    }
+
+    private void Run() {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(controlThrow * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = movement;
+    }
+}
